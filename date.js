212555ffb17fb9770b957fc600d8b27fc44ccb4a
Niklas <norr1000@gmail.com>
2018-03-20 15:35:36 +0100

    add date maker function that respect immutability

--- date.js
+++ date.js
@@ -6,8 +6,12 @@ var myDate = new Date();
 log('myDate = ',myDate);
 
 
-function makeNewDateFromReference(daysOffset, referenceDate) {
+function updateDate(daysOffset, referenceDate) {
 	return new Date (referenceDate.setTime(referenceDate.getTime() + daysOffset * 86400000 )); 
+}
+
+function makeNewDateFromReference(daysOffset, referenceDate) {
+	return new Date (new Date().setTime(referenceDate.getTime() + daysOffset * 86400000 )); 
 }
 
 function makeNewDate(daysOffset) {
@@ -15,9 +19,17 @@ function makeNewDate(daysOffset) {
 }
 
 log('newDate w/ func = ',makeNewDateFromReference(10,myDate)); 
-// should log 10 days ahead unless you change the value of myDate
+// should log 10 days ahead of current date unless you change the value of myDate
+// it does mutate the original date
 
 log('newDate w/ func = ',makeNewDate(2));
-// should log 2 days ahead
+// should log 2 days ahead of current date unless ...
+
+log('update date = ',updateDate(5,myDate)); 
+// should log 5 days ahead of current date unless...
+
+log('check myDate has been mutated', myDate);
+// should log 5 days ahead of current date unless...
+
 
 // Day time savings edge cases not taken into account
